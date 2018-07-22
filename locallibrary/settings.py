e21a9c3e499df1f93953de8cf2f8fd520aa0d638
Tino Truong <tino@tinotruong.com>
2018-07-22 13:43:16 -0700

    updated with scidereal.com

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['www.scidereal.com', '.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
